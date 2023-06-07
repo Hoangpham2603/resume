a05e41a9ef3cbce3e4221a20ba8297710a5823ce
Hoangpham2603 <kunpham2603@gmail.com>
2023-06-07 16:26:56 -0700

    finish header + toggle

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='mt-8'>
-      ạdkjasdh
+      ạdkjasdh 
     </div>
   )
 }
--- src/templates/HomeTemplates.jsx
+++ src/templates/HomeTemplates.jsx
@@ -1,58 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet, NavLink } from "react-router-dom";
 
 const HomeTemplates = () => {
+
+  //                      Toggle Menu
+    const [toggle, setToggle] = useState(false)
+
+
+
+    //                             Main
   return (
     <>
       <header className="header">
         <nav className="nav container">
           <NavLink to="" className={"nav__link active-link"}>
-            <h2 className="nav-name active-link">Hoàng Phạm</h2>
+            Hoang Pham
+            {/* <h2 className="nav__name active-link">Hoang Pham</h2> */}
           </NavLink>
 
-          <div className="nav__menu">
-            <ul className="nav__list flex">
-              <li>
-                <NavLink className={"nav__link active-link"}>
+          <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
+            <ul className="nav__list grid">
+              <li className="nav__item">
+                <NavLink to="" className={"nav__link active-link"}>
                   <i className="uil uil-estate  nav__icon "></i> Home
                 </NavLink>
               </li>
 
-              <li>
-                <NavLink className={"nav__link "}>
-                  <i className="uil uil-user nav__icon  "></i> About
+              <li className="nav__item">
+                <NavLink to="" className={"nav__link "}>
+                  <i className="uil uil-user nav__icon  "></i> <span>About</span>
                 </NavLink>
               </li>
 
-              <li>
-                <NavLink className={"nav__link "}>
+              <li className="nav__item">
+                <NavLink to="" className={"nav__link "}>
                   <i className="uil uil-file-alt nav__icon"></i> Skill
                 </NavLink>
               </li>
 
-              <li>
-                <NavLink className={"nav__link"}>
-                  <i className="uil uil-message nav__icon"></i> Contact
+              <li className="nav__item">
+                <NavLink  to="" className={"nav__link"}>
+                  <i  className="uil uil-message nav__icon"></i> Contact
                 </NavLink>
               </li>
 
-              <li>
-                <NavLink className={"nav__link"}>
-                  <i className="uil uil-message nav__icon"></i> Contact
-                </NavLink>
-              </li>
-
-              <li>
-                <NavLink className={"nav__link"}>
-                  <i className="uil uil-message nav__icon"></i> Contact
-                </NavLink>
-              </li>
+          
             </ul>
 
-            <i className="uil uil-times nav__close"></i>
+            <i onClick={() => {setToggle(!toggle)}} className="uil uil-times nav__close"></i>
           </div>
 
-          <div className="nav__toggle">
+          <div className="nav__toggle" onClick={() => {setToggle(!toggle)}}>
             <i className="uil uil-apps"></i>
           </div>
         </nav>
